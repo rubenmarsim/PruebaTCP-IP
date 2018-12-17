dd20763f1ca3e34000d3188ce68669a04bf9c2ae
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-12-17 17:54:26 +0100

    .

--- PruebaTCP-IP/PruebaTCP-IP/Program.cs
+++ PruebaTCP-IP/PruebaTCP-IP/Program.cs
@@ -10,42 +10,76 @@ namespace PruebaTCP_IP
 {
     class Program
     {
-        #region Variables e Instancias Globales
-        static int _PortN = 21;
-        static string _Server = "hola";
-        static TcpListener _Listener = new TcpListener(IPAddress.Any, _PortN);
-        static TcpClient _Client = _Listener.AcceptTcpClient();
-        static NetworkStream _NS = _Client.GetStream();
-        static byte[] _buffer = new byte[1024];
-        TcpClient tcpClient = new TcpClient(_Server, _PortN);
-        #endregion
         static void Main(string[] args)
         {
-            //int PortN = 21;
-            //TcpListener Listener = new TcpListener(IPAddress.Any, PortN);
-            //TcpClient Client = Listener.AcceptTcpClient();
-            //NetworkStream NS = Client.GetStream();
-            //byte[] buffer = new byte[1024];
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
 
-            _Listener.Start();
-            SendMessageReturntoSocketClient();
-            Console.ReadKey();
-        }
-        private static void AccionesServer()
-        {
-            //_Listener.Start();
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
 
-        }
-        private static void SendMessageReturntoSocketClient()
-        {
-            byte[] nouBuffer = Encoding.ASCII.GetBytes("Hola s2AM!!");
-            _NS.Write(nouBuffer, 0, nouBuffer.Length);
-        }
-        private static void PararTodo()
-        {
-            _NS.Close();
-            _Client.Close();
-            _Listener.Stop();
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also user server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
         }
     }
 }
